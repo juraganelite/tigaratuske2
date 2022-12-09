47512529ba57f1613e0f7e56e3863680424f4493
juraganelite <101795987+juraganelite@users.noreply.github.com>
2022-12-09 20:21:20 +0700

    Create 300a250.js

---
+++ 300a250.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '9b12060f53801f2915137822cb22aa41',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://bringsconserve.com/9b12060f53801f2915137822cb22aa41/invoke.js"></scr' + 'ipt>');
